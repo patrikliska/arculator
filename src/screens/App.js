109e598265ff025b161ea3b443360762ff0a4136
Patrik <Soboj500@gmail.com>
2020-01-17 20:15:41 +0100

    initial commit

---
+++ src/screens/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/App.css";
+
+import NavigationMenu from "../components/NavigationMenu";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <NavigationMenu />
+            </header>
+        </div>
+    );
+}
+
+export default App;
