65d202a2c006932f71c3b6faf6c920049a9b1462
Patrik <Soboj500@gmail.com>
2020-01-17 20:30:37 +0100

    Updates

---
+++ precache-manifest.6044b0db16c44773d8cabdb8769f8b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b879b5a7583b1b870f4235dbcb979114",
+    "url": "/arculator/index.html"
+  },
+  {
+    "revision": "004ddf7b512d224ac041",
+    "url": "/arculator/static/css/main.75cddc36.chunk.css"
+  },
+  {
+    "revision": "05a4436ac2ac9be6f997",
+    "url": "/arculator/static/js/2.8777b05c.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/arculator/static/js/2.8777b05c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "004ddf7b512d224ac041",
+    "url": "/arculator/static/js/main.0a39cf4b.chunk.js"
+  },
+  {
+    "revision": "84a84cb66c6d4ea368d8",
+    "url": "/arculator/static/js/runtime-main.bc30be7b.js"
+  }
+]);
